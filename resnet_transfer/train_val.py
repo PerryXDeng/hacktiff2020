a35caef54f426f303bb4ce2d49ba323dfb2b5bc3
Bowald <dtb6369@rit.edu>
2020-01-25 21:01:21 -0500

    making code not hot garbage

--- resnet_transfer/train_val.py
+++ resnet_transfer/train_val.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -
 from __future__ import print_function, division
 
-
 import torch
+import torch.nn as nn
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
+import torch.optim as optim
+from torch.optim import lr_scheduler
 
-
-from ..train import train_model
-from .config import *
+from single_view_train import train_model
+from resnet_transfer.config import *
 
 #data_dir = 'data/hymenoptera_data' # has train, val subdirs
 
@@ -22,24 +23,25 @@ from .config import *
 
 # change it to initialize with PyTorch Dataset instance, should spit out image and measurements
 dataset = None 
-total = len(dataset))
+total = len(dataset)
 
 
-phases = ['train', 'val'] # change it to just train if val is no longer needed
+phases = ['train'] # change it to just train if val is no longer needed
 
-if len(phases) == 2:
-  random_train_indices = np.random.choice(np.arange(total), size=(total//5)*4, replace=False)
-
-  random_val_indices = np.ones(total, dtype=bool)
-  random_val_indices[random_train_indices] = False
-  random_val_indices = np.nonzero(random_val_indices)
-
-  split_indices = {'train': random_train_indices, 'val': random_val_indices}
-
-  partitions = {x: torch.utils.Subset(dataset, split_indices[x])
-                for x in phases}
-else:
-  partitions = {'train':dataset}
+#if len(phases) == 2:
+#  random_train_indices = np.random.choice(np.arange(total), size=(total//5)*4, replace=False)
+#
+#  random_val_indices = np.ones(total, dtype=bool)
+#  random_val_indices[random_train_indices] = False
+# random_val_indices = np.nonzero(random_val_indices)
+#
+#  split_indices = {'train': random_train_indices, 'val': random_val_indices}
+#
+#  partitions = {}''train
+#  partitions = {x: torch.utils.Subset(dataset, split_indices[x])
+#                for x in phases}
+#else:
+partitions = {'train':dataset}
 
 
 dataloaders = {x: torch.utils.data.DataLoader(partitions[x], batch_size=batch_size,
@@ -62,12 +64,12 @@ if fix_pretrained_weights:
 
 # replace fc layer and add linear layer
 class module_with_concat_linear_layer(nn.Module):
-  def __init__(self, pretrained, metadata_dim, output_dim)
+  def __init__(self, pretrained, metadata_dim, output_dim):
     super(module_with_concat_linear_layer, self).__init__()
     self.pretrained = pretrained
 
     num_ftrs = pretrained.fc.in_features + metadata_dim
-    self.fc = nn.Softmax(num_ftrs, num_trained_hidden_neurons)
+    self.fc = nn.Softmax(num_ftrs, num_new_hidden_neurons)
     pretrained.fc = nn.Identity()
 
     self.linear = nn.modules.Linear(num_new_hidden_neurons, output_dim)
@@ -77,7 +79,7 @@ class module_with_concat_linear_layer(nn.Module):
     x = torch.cat((x, metadata), 1) # 1 because 0 is for batch dimension
     return self.linear(self.fc(x))
 
-model.ft = module_with_concat_linear_layer(self.pretrained, num_metadata, num_measures)
+model_ft = module_with_concat_linear_layer(model_pretrained, num_metadata, num_measures)
 
 
 ######################################################################
@@ -98,8 +100,5 @@ else:
   optimizer = optim.Adam(model_ft.parameters(), lr=learning_rate)
   scheduler = None
 
-
 model_ft = train_model(dataloaders, model_ft, criterion, optimizer, scheduler,
-                       num_epochs=fine_tune_epochs)
-
-
+                       num_epochs=fine_tune_num_epochs)
